7e6557b2719d5241c1c68596dcf5871fb663cdc8
glaurios <adrienglaurios@gmail.com>
2025-11-24 20:56:41 +0000

    newkowri

--- src/config/kowri.js
+++ src/config/kowri.js
@@ -1,5 +1,5 @@
 export const kowriConfig = {
-  baseUrl: process.env.KOWRI_BASE_URL || "https://api.kowri.com/v1",
+ baseUrl: process.env.KOWRI_BASE_URL || "https://posapi.kowri.app/",
   apiKey: process.env.KOWRI_API_KEY,
   merchantId: process.env.KOWRI_MERCHANT_ID,
   webhookSecret: process.env.KOWRI_WEBHOOK_SECRET,
